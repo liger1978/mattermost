d39a51702fa10881a1dce8852588890909fde503
grainger@gmail.com <grainger@gmail.com>
2016-01-13 22:42:25 +0000

    Troubleshoot data_dir

--- manifests/params.pp
+++ manifests/params.pp
@@ -20,7 +20,7 @@ class mattermost::params {
   $install_service = true
   $manage_service = true
   
-  if ($override_options['FileSettings'] != undef) {
+  if ( ! empty($override_options['FileSettings']) ) {
     notify { "here 1":
       message => 'here 1',
     }
